4340f95ff39a9566599f93143ca56fae87c65f3d
jalberto <jason.alberto.dev@gmail.com>
2025-03-27 17:06:32 -0400

    Add button variants and their respected styles and colour selecting functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,14 @@ export default function Home() {
   };
 
   return (
-    <>
+    <div style={{margin: "20px"}}>
       <Button onClick={handleClick}>Button CSV</Button>
-    </>
+      <Button rounded={true}  onClick={handleClick}>Button CSV</Button>
+      <Button variant="secondary" onClick={handleClick}>Button CSV</Button>
+      <Button  variant="outline" onClick={handleClick}>Button CSV</Button>
+      <Button color="#008000" variant="destructive" onClick={handleClick}>Button CSV</Button>
+      <Button color="#008000" onClick={handleClick}>Button CSV</Button>
+      <Button variant="secondary" color="#008000" onClick={handleClick}>Button CSV</Button>
+    </div>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,44 @@
 "use client";
 import styles from "./button.module.scss";
+import { lightenColor } from "../../utils/colorUtil";
 
 interface ButtonProps {
-  children: React.ReactNode;
-  onClick?: () => void;
+    children: React.ReactNode;
+    onClick?: () => void;
+    // Variants alter the button's style, no value is the default style for a bold colour button
+    variant?: "primary" | "secondary" | "outline" | "destructive";
+    // Default color is #463ACB, color accepts a HEX value and will customize the button color
+    color?: string;
+    // The lighten prop will overide the default lighghening value of 8
+    lighten?: number;
+    // This will round the button's corners to a rounded, pill-like shape
+    rounded?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({
-  children,
-  onClick,
+    children,
+    onClick,
+    variant= "primary",
+    color = "#463ACB",
+    lighten = 8,
+    rounded = false,
 }) => {
+
+    const btnColorsStyles: {[key: string]: string} = {
+        "--btn-primary-color": color,
+        "--btn-primary-color-hover": lightenColor(color, lighten),
+        "--btn-secondary-color": "#FFF",
+        "--btn-secondary-color-hover": color,
+        "--btn-border-radius": rounded ? "9999px" : "4px",
+    };
+
   return (
-    <button className={styles.btn} onClick={onClick}>
-      {children}
+    <button 
+        className={`${styles.btn} ${variant && styles[variant]}`} 
+        onClick={onClick} 
+        style={btnColorsStyles}
+    >
+        {children}
     </button>
   );
 }
