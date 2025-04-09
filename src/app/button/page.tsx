b01908c5ead82eb712380d3292aab8363580bab2
jalberto <jason.alberto.dev@gmail.com>
2025-04-08 21:29:24 -0400

    Add button intro

--- src/app/button/page.tsx
+++ src/app/button/page.tsx
@@ -19,9 +19,19 @@ export default function ButtonDocs() {
   return (
     <div>
       <section>
+        <h1>Button</h1>
+        <p>
+        The Button component is a versatile interactive UI element that responds to user input. 
+        It’s used to trigger actions within your application and can be customized in style, size, and behavior.
+        </p>
+      </section>
+      <section>
         <h2>Styling</h2>
         <p>
-            You can easily customize the button's appearance using inline styles by passing a `style` prop with standard CSS properties. This allows you to set properties like background color, padding, font size, and more directly within your JSX:
+          You can easily customize the button's appearance using inline styles
+          by passing a `style` prop with standard CSS properties. This allows
+          you to set properties like background color, padding, font size, and
+          more directly within your JSX:
         </p>
         <CodeBlock className="code-content" code={sampleCode} />
       </section>
