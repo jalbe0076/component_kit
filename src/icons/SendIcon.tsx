6a7c2d16c5eedf255092a375b15d2dd6015f5d65
jalberto <jason.alberto.dev@gmail.com>
2025-04-09 23:19:59 -0400

    Add delete and send icon

---
+++ src/icons/SendIcon.tsx
@@ -0,0 +1,14 @@
+const SendIcon = () => {
+  return (
+    <svg
+      height="16"
+      viewBox="0 -960 960 960"
+      width="16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="m120-160v-640l760 320zm80-120 474-200-474-200v140l240 60-240 60zm0 0v-400z" />
+    </svg>
+  );
+};
+
+export default SendIcon;
