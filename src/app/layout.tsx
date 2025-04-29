c616298f28297741c86ca90e4d315b68aef2ca3a
jalberto <jason.alberto.dev@gmail.com>
2025-04-29 15:05:06 -0400

    Remove reading url path and set bg image according to system preferences only

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,59 +1,36 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import { useSearchParams } from "next/navigation"; // <--- ADD THIS
+import React, { useEffect } from "react";
 import "./globals.scss";
 import Sidebar from "./Sidebar";
-import BackgroundPattern from "@/icons/BackgroundPattern";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const searchParams = useSearchParams();
-  const [isDark, setIsDark] = useState<boolean>(false);
 
   useEffect(() => {
-    const themeParam = searchParams.get("theme"); // <-- get 'theme' from URL, required for portfolio iframe element to manually change the theme
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-
-    if (themeParam === "dark") {
-      setIsDark(true);
-    } else if (themeParam === "light") {
-      setIsDark(false);
-    } else {
-      setIsDark(mediaQuery.matches);
-
-      const handleChange = (e: MediaQueryListEvent) => {
-        setIsDark(e.matches);
-      };
-      mediaQuery.addEventListener("change", handleChange);
-
-      return () => {
-        mediaQuery.removeEventListener("change", handleChange);
-      };
-    }
-  }, [searchParams]);
-
-  useEffect(() => {
-    generateThemedSVG(isDark ? "dark" : "light");
-  }, [isDark]);
-
-  const generateThemedSVG = (theme: string) => {
-    const mainBgColor =
-      theme === "dark" ? "rgb(58, 63, 55)" : "rgb(240, 241, 241)";
-    const secondaryBgColor =
-      theme === "dark" ? "rgb(42, 46, 40)" : "rgb(225, 225, 225)";
-    const svgContent = BackgroundPattern(mainBgColor, secondaryBgColor);
-
     const container = document.getElementById("background-container");
+  
+    const updateBackground = () => {
+      const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      const bgUrl = isDark
+        ? "./svg/background-dark.svg"
+        : "./svg/background-light.svg";
 
-    if (container) {
-      container.style.background = `url("data:image/svg+xml,${encodeURIComponent(
-        svgContent
-      )}")`;
-    }
-  };
+      if (container) {
+        container.style.backgroundImage = `url("${bgUrl}")`;
+      }
+    };
+  
+    updateBackground();  
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    mediaQuery.addEventListener("change", updateBackground);
+  
+    return () => {
+      mediaQuery.removeEventListener("change", updateBackground);
+    };
+  }, []);
 
   return (
     <html lang="en">
